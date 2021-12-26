0a005f6689015edfe91a1fe26062d5086556fa3e
landaal-ict <89043910+landaal-ict@users.noreply.github.com>
2021-12-26 18:00:04 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ echo Install Mikrotik Backup requirements
 
 sudo apt install python3 python3-pip sshpass 
 
+python3 -m pip install ansible
+
 pip3 install paramiko
 
 ansible-galaxy collection install community.general
